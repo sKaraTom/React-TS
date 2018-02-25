173a4438735f680aca3584197c52dd62ec4e3444
sKaraTom <7clauss@gmail.com>
2018-02-25 17:53:30 +0100

    test class et interface

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,10 +1,24 @@
 import * as React from 'react';
-import { HelloProps } from './Hello';
+import { User } from './model/User';
 
-export interface HelloProps { compiler: string; framework: string; }
+interface HelloProps { compiler: string; framework: string; }
 
-export class Hello extends React.Component<HelloProps, {}> {
+interface State {
+    user:User;
+  }
+
+export class Hello extends React.Component<HelloProps, State> {
+
+    constructor(props) {
+        super(props);
+        this.state = { user: new User()};
+    }
+
     render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+    return (
+    <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>
+    
+    
+    );
     }
 }
---
+++ src/model/User.ts
@@ -0,0 +1,17 @@
+
+
+export class User {
+
+    public name:string;
+    public firstName:string;
+    public login:string;
+    public age:number;
+
+    constructor() {
+        this.name = "";
+        this.firstName = "";
+        this.login = "";
+        this.age = 0;
+    }
+
+}
